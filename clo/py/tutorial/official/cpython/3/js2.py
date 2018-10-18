69d3e9c4199cd002fff520231152ed31bbdce413
劉東熠 <absente@live.cn>
2018-10-19 00:23:14 +0800

    split js.py

---
+++ clo/py/tutorial/official/cpython/3/js2.py
@@ -0,0 +1,39 @@
+
+'''3.1.2. Strings
+>>> 'spam eggs'  # single quotes
+'spam eggs'
+>>> 'doesn\'t'  # use \' to escape the single quote...
+"doesn't"
+>>> "doesn't"  # ...or use double quotes instead
+"doesn't"
+>>> '"Yes," they said.'
+'"Yes," they said.'
+>>> "\"Yes,\" they said."
+'"Yes," they said.'
+>>> '"Isn\'t," they said.'
+'"Isn\'t," they said.'
+'''
+
+'''
+'''
+
+'''
+'''
+
+'''
+'''
+
+'''
+'''
+
+'''
+'''
+
+'''
+'''
+
+'''
+'''
+
+'''
+'''
